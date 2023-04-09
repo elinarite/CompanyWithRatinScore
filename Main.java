6ac69911133240f38eb4f98fa4e87e240c2cf085
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-04-09 21:36:20 +0200

    тест

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package TaskWork.BasicForAll4;
 import java.util.Scanner;
 
 public class Main {
+// TODO Отлично, что здесь у нас тольок один метод. Чтобы было совсем чисто - удаляй ненужные пропуски строк,
+    // например у тебя две пустые строки в мейн
     public static void main(String[] args) {
 
 
